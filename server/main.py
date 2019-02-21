9afa3cf5e537f6407e654971d49b1252139e4f15
Vadym Hevlich <vege1wgw@gmail.com>
2019-02-21 18:21:56 +0200

    stream from flv video file added, check endpoinds calls added

--- server/main.py
+++ server/main.py
@@ -1,13 +1,43 @@
+import subprocess
+
+import requests
 from flask import Flask, request, Response
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-@app.route('/on_publish', methods=['POST'])
-def index():
-    print(request.form)
+@app.route('/file_stream/<filename>', methods=['GET'])
+def file_stream(filename):
+    command = 'ffmpeg -re -i /var/video-records/{filename}.flv ' \
+              '-acodec copy -vcodec copy -f flv rtmp://nginx:1935/stream/{filename}'\
+        .format(filename=filename)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output, error)
     return Response(status=201)
+
+
+@app.route('/stream-action/<action>', methods=['POST'])
+def stream_action(action):
+    bad_response = Response(status=400)
+    response = Response(status=201)
+    user_id = request.form.get('user_id')
+
+    if action == 'on_publish':
+        r = requests.post('http://django-server-domain/check-publish-stream-allowed',
+                          data={'user_id': user_id})
+        if r.status_code != 200:
+            return bad_response
+    elif action == 'on_publish_done':
+        return bad_response
+    elif action == 'on_play':
+        r = requests.post('http://django-server-domain/check-play-stream-allowed',
+                          data={'user_id': user_id})
+        if r.status_code != 200:
+            return bad_response
+
+    return response
 
 
 if __name__ == '__main__':
